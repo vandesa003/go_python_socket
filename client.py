0e77aa6beea358b3b6402d7de451fb25d08031c3
Bohang Li <vandesa003@gmail.com>
2021-09-07 00:28:12 +0800

    socket connection

--- client.py
+++ client.py
@@ -1,10 +1,9 @@
 import socket
 
-HOST = 'localhost'  # The server's hostname or IP address
-PORT = 8080         # The port used by the server
+addr = "test.socket"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+    s.connect(addr)
     s.sendall(b'Hello, world\n')
     data = s.recv(1024)
 
